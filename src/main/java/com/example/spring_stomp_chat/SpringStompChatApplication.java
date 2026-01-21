ef1423abb637faab7fdfc810e609c4662f4c78aa
YooMinWoo <alsn0527@naver.com>
2026-01-22 00:19:43 +0900

    first commit

---
+++ src/main/java/com/example/spring_stomp_chat/SpringStompChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_stomp_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_stomp_chat/SpringStompChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_stomp_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
